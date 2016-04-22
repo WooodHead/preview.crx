4fc1a1f074322c874c5fc9eac087bdf04db466f3
Brian Norton <brian.nort@gmail.com>
2016-04-22 11:52:00 -0700

    Allow dashboard.trychameleon.com to update the stored settings.

--- code/js/content.js
+++ code/js/content.js
@@ -1,39 +1,75 @@
 ;(function() {
-  var settings = ['accountUrl', 'accountToken'];
+  var settings = ['accountUrl', 'accountToken', 'accountUrlAt', 'accountTokenAt'],
+    onDashboard = /dashboard\.trychameleon/.test(document.location.hostname),
+    href = document.location.href,
+    accountToken, accountUrl, urlUpdated, tokenUpdated;
+
+  if(onDashboard) {
+    document.addEventListener('dashboard-data', updateSettings);
+
+    addScript('window.dashboard && (dashboard.adminPreview = true) && dashboard.adminPreviewCheck();');
+  }
 
   chrome.storage.local.get(settings, function(o) {
-    if(!o.accountUrl || !o.accountToken) {
+    accountToken = o.accountToken;
+    accountUrl = o.accountUrl;
+    urlUpdated = o.accountUrlAt;
+    tokenUpdated = o.accountTokenAt;
+
+    if(!onDashboard) {
+      start();
+    }
+  });
+
+  function start() {
+    if (!accountToken || !accountUrl) {
       return;
-    } else if(document.location.hostname.indexOf(o.accountUrl) === -1) {
+    } else if (href.indexOf(accountUrl.replace(/https?:\/\//, '')) === -1) {
       return;
     }
 
-    var string = "(function(doc,win) { \
-      var chmln = 'chmln', \
-        names = 'setup identify alias track set show on off custom help _data'.split(' '); \
- \
-      win[chmln] || (win[chmln] = {}); \
-      win[chmln].accountToken = '"+o.accountToken+"'; \
-      win[chmln].adminPreview = true; \
- \
-      for(var i = 0; i<names.length; i++) { \
-        (function() { \
-          var calls = win[chmln][names[i]+'_a'] = []; \
-          win[chmln][names[i]] = function() { \
-            calls.push(arguments); \
-          }; \
-        })(); \
-      } \
- \
+    addScript(" \
+    !function(doc,win,root) { if(root) { return; } \
+      win.chmln = { accountToken: '"+accountToken+"', adminPreview: true, identify: function() {} }; \
+    \
       var script = doc.createElement('script'); \
-      script.src = 'https://fast.trychameleon.com/messo/'+'"+o.accountToken+"'+'/messo.min.js'; \
+      script.src = 'https://fast.trychameleon.com/messo/'+win.chmln.accountToken+'/messo.min.js'; \
       script.async = true; \
       doc.head.appendChild(script); \
-    })(document,window);";
+    }(document,window,window.chmln);");
+  }
 
+  function addScript(string) {
     var script = document.createElement('script');
     script.setAttribute('type', 'text/javascript');
     script.innerHTML = string;
     document.head.appendChild(script);
-  });
+
+    setTimeout(function() { document.head.removeChild(script); }, 500);
+  }
+
+  function updateSettings() {
+    if(urlUpdated || tokenUpdated) {
+      return;
+    }
+
+    var account = JSON.parse(document.querySelector('#dashboard-data').getAttribute('data-account')),
+      token = account.tokens[account.tokens.length-1],
+      url = account.url,
+      options = {};
+
+    if(!tokenUpdated && token && accountToken !== token) {
+      accountToken = options.accountToken = token;
+    }
+
+    if(!urlUpdated && url && accountUrl !== url) {
+      accountUrl = options.accountUrl = url;
+    }
+
+    if(Object.keys(options).length) {
+      console.log('Automatically setting up the Chameleon Preview configuration token and url.');
+
+      chrome.storage.local.set(options, function() { });
+    }
+  }
 })();
--- code/js/modules/form.js
+++ code/js/modules/form.js
@@ -9,11 +9,12 @@ module.exports = function(callback) {
   $(function() {
     var $form = $('#popup-form'),
       $token = $('#token'),
-      $url = $('#url');
+      $url = $('#url'),
+      originalToken, originalUrl;
 
     chrome.storage.local.get(['accountToken', 'accountUrl'], function(o) {
-      o.accountToken && $token.val(o.accountToken);
-      o.accountUrl && $url.val(o.accountUrl) && setTimeout(function() { $url.focus(); }, 10);
+      o.accountToken && $token.val(o.accountToken) && (originalToken = o.accountToken);
+      o.accountUrl && $url.val(o.accountUrl) && (originalUrl = o.accountUrl) && setTimeout(function() { $url.focus(); }, 10) && setTimeout(function() { $url.focus(); }, 50);
     });
 
     $form.on('submit', function(e) {
@@ -22,15 +23,17 @@ module.exports = function(callback) {
       var token = $token.val(),
         url = $url.val(),
         options = {},
-        close = function() { setTimeout(window.close.bind(window), 350) };
+        close = function(t) { setTimeout(window.close.bind(window), t || 850) };
 
-      token && (options.accountToken = token);
-      url   && (options.accountUrl = url);
+      token && (token !== originalToken) && (options.accountToken = token) && (options.accountTokenAt = new Date().toISOString());
+      url   && (url !== originalUrl) && (options.accountUrl = url) && (options.accountUrlAt = new Date().toISOString());
 
       if(Object.keys(options).length) {
         chrome.storage.local.set(options, close);
+
+        $('button').text('Saved...');
       } else {
-        close();
+        close(150);
       }
     });
   });
